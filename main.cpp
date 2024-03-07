04f18fc7480b1dd6544b5c97e52b050ffce1269e
huukhai249 <khaikn249@gmail.com>
2024-03-07 22:18:11 +0900

    this is the first commit from computer on lab

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 // this is main function to solve Heat Steady Transfer
+// Include "petscsles.h" so that we can use SLES solvers. Note that this file
+// automatically includes:
+//      petsc.h - base PETSc routines 
+//      petscvec.h - vectors
+//      petscsys.h - system routines 
+//      petscmat.h - matrices
+//      petscis.h - index sets 
+//      petscksp.h - Krylov subspace methods
+//      petscviewer.h - viewers 
+//      petscpc.h - preconditioners 
+
 #include<iostream>
 using namespace std;
 
